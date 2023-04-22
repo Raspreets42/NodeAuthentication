41a787f89a22ef0c8f88b655f014ec272c16fe99
Raspreet singh <raspreets89@gmail.com>
2023-04-23 01:05:12 +0530

    changed the response

--- index.js
+++ index.js
@@ -1,117 +1,126 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const connectDB = require("./db/connect");
 
-const bcrypt = require('bcrypt');
-const User = require('./model/schema');
+const bcrypt = require("bcrypt");
+const User = require("./model/schema");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const app = express();
-app.use('/' , express.static(path.join(__dirname,'static')));
-app.use(cors( {origin: '*' }));
+app.use(cors({ origin: "*" }));
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/api/changepassword' , async (req,res) => {
-    const { userNewPassword , userCNewPassword  , token} = req.body;
-    if(userNewPassword !== userCNewPassword){
-        res.json({status:'error' , error: 'Password does not got matched.'})
-    }
-    try {
-        const user = jwt.verify(token,JWT_SECRET);
-        const _id = user.id;
-        const hashedPass = await bcrypt.hash(userNewPassword , 10);
-        const hashedCPass = await bcrypt.hash(userCNewPassword , 10);
-        console.log(user);
+app.post("/api/changepassword", async (req, res) => {
+  const { userNewPassword, userCNewPassword, token } = req.body;
+  if (userNewPassword !== userCNewPassword) {
+    res.status(404).json({ status: false, msg: "Password does not got matched." });
+  }
+  try {
+    const user = jwt.verify(token, JWT_SECRET);
+    const _id = user.id;
+    const hashedPass = await bcrypt.hash(userNewPassword, 10);
+    const hashedCPass = await bcrypt.hash(userCNewPassword, 10);
+    console.log(user);
 
-        await User.updateOne({_id},{
-            $set : {userPassword : hashedPass},
-            $set : {userCPassword : hashedCPass},
-        })
-        res.json({status: 'ok' , msg: 'Password changed successfully'})
-    } catch (error) {
-        res.json({status: 'error', error: 'Failed to respond'});
-    }
-
+    await User.updateOne(
+      { _id },
+      {
+        $set: { 
+            userPassword: hashedPass,
+            userCPassword: hashedCPass
+        },
+      }
+    );
+    res.status(200).json({ status: true, msg: "Password changed successfully" });
+  } catch (error) {
+    res.status(404).json({ status: false, msg: "Failed to respond" });
+  }
 });
 
-app.post('/api/login' , async (req,res) => {
-    const { userEmail , userPassword } = req.body;
+app.post("/api/login", async (req, res) => {
+  const { userEmail, userPassword } = req.body;
 
-    const user = await User.findOne({ userEmail });
-    
-    if(!user){
-        return res.json({status:'error',error: 'Invalid User email or/and password'});
-    }
+  const user = await User.findOne({ userEmail });
 
-    if(await bcrypt.compare(userPassword , user.userPassword)){
+  if (!user) {
+    return res.status(404).json({
+      status: false,
+      msg: "Invalid User email or/and password",
+    });
+  }
 
-        const token = jwt.sign(
-            {
-                id: user._id,
-                userEmail: user.userEmail,
-            },
-            JWT_SECRET
-        )
-        localStorage.setItem('token',token);
-        return res.json({status:'Ok' , msg: 'User logged-in' , token: token});
-    }
+  if (await bcrypt.compare(userPassword, user.userPassword)) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+        userEmail: user.userEmail,
+      },
+      JWT_SECRET
+    );
+    return res.status(200).json({ status: true, msg: "User logged-in successfully", token: token });
+  }
 
-    res.json({status:'error',error: 'Invalid User email or/and password'});
-})
+  res.status(404).json({ status: false, msg: "Invalid User email or/and password" });
+}); 
 
-app.post('/api/register' , async (req,res) => {
-    
-    const { userName , userEmail , userPhone , userPassword , userCPassword } = req.body;
+app.post("/api/register", async (req, res) => {
+  const { userName, userEmail, userPhone, userPassword, userCPassword } =
+    req.body;
 
-    if (userName === '' || userEmail === '' || userPhone === '' || userPassword === '' || userCPassword === '') {
-        res.json({
-            status: 500,
-            message: 'Please fill all the required fields'
-        })
-        return
-    }else if (userPassword !== userCPassword) {
-        res.json({
-            status: 401,
-            message: 'Password does not matches'
-        })
-        return
-    }
+  if (
+    userName === "" ||
+    userEmail === "" ||
+    userPhone === "" ||
+    userPassword === "" ||
+    userCPassword === ""
+  ) {
+    res.status(404).json({
+      status: 500,
+      msg: "Please fill all the required fields",
+    });
+    return;
+  } else if (userPassword !== userCPassword) {
+    res.status(404).json({
+      status: 401,
+      msg: "Password does not matches",
+    });
+    return;
+  }
 
-    const userHashedPassword = await bcrypt.hash(userPassword , 10);
-    const userHashedCPassword = await bcrypt.hash(userCPassword , 10);
-    
-    try {
-        const resp = await User.create({ 
-            userName ,
-            userEmail ,
-            userPhone ,
-            userPassword: userHashedPassword ,
-            userCPassword: userHashedCPassword
-        } );
-        console.log('resp : ' , resp);
-        res.json({status:'success' , message: 'Successfully Registered'});
-    } catch (err) {
-        res.json({status: 'Error' , message: err.message });
-    }
-})
+  const userHashedPassword = await bcrypt.hash(userPassword, 10);
+  const userHashedCPassword = await bcrypt.hash(userCPassword, 10);
 
-const start = async() => {
-    try {
-        await connectDB();
-        app.listen(process.env.PORT, () => {
-            console.log(`server is running on port ${process.env.PORT}`);
-        })
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    const resp = await User.create({
+      userName,
+      userEmail,
+      userPhone,
+      userPassword: userHashedPassword,
+      userCPassword: userHashedCPassword,
+    });
+    console.log("resp : ", resp);
+    res.status(2000).json({ status: true, msg: "Successfully Registered" });
+  } catch (err) {
+    res.status(404).json({ status: false, msg: err.message });
+  }
+});
+
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(process.env.PORT, () => {
+      console.log(`server is running on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 start();
